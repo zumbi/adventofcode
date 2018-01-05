d4c85d30ceccb14004e1598ff3f958eb15639813
Ricardo Ribalda Delgado <ricardo.ribalda@gmail.com>
2018-01-05 22:53:48 +0100

    2016: day 18

---
+++ 2016/18/step2.py
@@ -0,0 +1,25 @@
+import sys
+
+lines = 400000
+is_trap = [[False,False,True],[True,False,False],[False,True,True],[True,True,False]]
+line = sys.stdin.readline()[:-1]
+safes = [i=="." for i in line]
+
+s = sum(safes)
+for lines in range(lines-1):
+ if (lines % 1000)==0:
+     print lines
+ safes2= [True] * len(safes)
+ for i in range(len(safes)):
+    tri = [True, True, True]
+    if i != 0:
+        tri[0] = safes[i-1]
+    tri[1] = safes[i]
+    if i != (len(safes)-1):
+        tri[2] = safes[i+1]
+    if tri in is_trap:
+        safes2[i] = False
+ safes = safes2
+ s += sum(safes)
+
+print s
