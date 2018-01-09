3f5b5cf9a24d8145a4a51d8bf1d27dbf7038854b
Ricardo Ribalda Delgado <ricardo.ribalda@gmail.com>
2018-01-09 01:00:05 +0100

    2016: day 22 step 2

--- 2016/22/step2.py
+++ 2016/22/step2.py
@@ -1,6 +1,5 @@
 import sys
 import copy
-import cPickle as pickle
 
 X = 38
 Y = 28
@@ -27,63 +26,70 @@ for l in sys.stdin.readlines()[2:]:
     size[y][x] = si
 
 def h(a,b):
-    return pickle.dumps([a,b])
+    return a[0] |  a[1] <<8 | b[0] <<16 | b[1] << 24
 
-def movements(used):
-    movements = list()
+def movements(used,zero):
+    mov = list()
     pos = [[-1,0],[1,0],[0,-1],[0,1]]
+    x = zero[1]
+    y = zero[0]
+    for p in pos:
+        y0 = zero[0]+p[0]
+        x0 = zero[1]+p[1]
+        if x0<0 or x0 >=X or y0<0 or y0 >=Y:
+            continue
+        if used[y0][x0] <= size[y][x]:
+            m = [[y0,x0] ,[y,x]]
+            mov += [m]
+
+    return mov
+
+def findz(used):
     for y in range(Y):
         for x in range(X):
-            for p in pos:
-                y0 = y+p[0]
-                x0 = x+p[1]
-                if x0<0 or x0 >=X or y0<0 or y0 >=Y:
-                    continue
-                if (used[y][x] + used[y0][x0]) <= size[y0][x0]:
-                    m = [[y,x] ,[y0,x0]]
-                    movements += [m]
-
-    return movements
+            if used[y][x] == 0:
+                return [y,x]
 
 data = [0,X-1]
-state = [data,used,0]
+ze = findz(used)
+state = [data,ze,used,0]
 
 work =[state]
 
 visited = dict()
+ha = h(data,ze)
+visited[ha] = True
+
 last_step = 0
 while len(work) > 0:
     s =work[0]
     work = work[1:]
 
-    [data,used,steps] = s
-    ha = h(used,data)
-    if ha in visited:
-        continue
-    visited[ha] = True
+    [data,zero,used,steps] = s
 
     if steps > last_step:
         last_step = steps
-        print (steps, len(work))
-    #print s
+        print (steps, len(work), zero, data)
 
     if data == [0,0]:
         print steps
         break
 
-    mov = movements(used)
+    mov = movements(used,zero)
     for m in mov:
         #print m
         s2 = copy.deepcopy(s)
-        [data,used,steps] = s2
-        used[m[1][0]][m[1][1]] += used[m[0][0]][m[0][1]]
+        [data,zero,used,steps] = s2
+        used[m[1][0]][m[1][1]] = used[m[0][0]][m[0][1]]
         used[m[0][0]][m[0][1]] = 0
         if [m[0][0],m[0][1]] == data:
             data = [m[1][0],m[1][1]]
-        ha = h(used,data)
+        zero = [m[0][0],m[0][1]]
+        ha = h(data,zero)
         if ha in visited:
             continue
-        s2 = [data,used,steps+1]
+        visited[ha] = True
+        s2 = [data,zero,used,steps+1]
         work += [s2]
 
 
