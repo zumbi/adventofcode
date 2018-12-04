0d70eb4263a94ea6c2e9579fee6456b18b94d808
Ricardo Ribalda Delgado <ricardo@ribalda.com>
2018-12-04 23:09:59 +0100

    2018/4: Simplify code
    Remove date parsing, not needed

--- 2018/4/step1_2.py
+++ 2018/4/step1_2.py
@@ -1,57 +1,39 @@
 import sys
-import datetime
 
-def get_day(a):
-    return a.strftime("%m%d")
-
-def date_dt(a):
+def date_num(a):
     a = a.split('] ')
-    dt = datetime.datetime.strptime(a[0][6:], "%m-%d %H:%M")
-    data = a[1].split()
-    if data[0] == "Guard":
-        if dt.hour == 23:
-            dt += datetime.timedelta(days=1)
-    return dt
+    a = a[0]
+    a = a.replace(" ","")
+    a = a.replace("[","")
+    a = a.replace(":","")
+    a = a.replace("-","")
+    return int(a)
 
 #Sort lines
 lines = sys.stdin.readlines()
-lines = sorted(lines, key=date_dt)
+lines = sorted(lines, key=date_num)
 
-
-#Find dates
-dates = set()
-for l in lines:
-    dt = date_dt(l)
-    dates.add(get_day((date_dt(l))))
-
-#Create table
-table=dict()
-for d in dates:
-    table[d] = 60 * [0] + [0]
+work_time = dict()
+guard = 0
 
 #Fill table
 for l in lines:
-    dt = date_dt(l)
     data = l.split('] ')[1]
     data = data.split()
     if data[0] == "Guard":
-        table[get_day(dt)][60] = int(data[1][1:])
+        guard = int(data[1][1:])
+        if guard not in work_time:
+            work_time[guard] = [0] * 60
         continue
+    minute = date_num(l) % 100
     if data[0] == "wakes":
-        for i in range(dt.minute,60):
-            table[get_day(dt)][i] = 0
+        for i in range(minute,60):
+            work_time[guard][i] -= 1 
         continue
     if data[0] == "falls":
-        for i in range(dt.minute,60):
-            table[get_day(dt)][i] = 1
-
-#sum data
-work_time = dict()
-for t in table:
-    if table[t][60] not in work_time:
-        work_time[table[t][60]] = [0] * 60
-    for i in range(60):
-        work_time[table[t][60]][i] += table[t][i]
+        for i in range(minute,60):
+            work_time[guard][i] += 1
+        continue
 
 #Step 1
 #sum_data
