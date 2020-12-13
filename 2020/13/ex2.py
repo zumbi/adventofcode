3268db28dc2aea6cabc62047131301b4696fd24d
Ricardo Ribalda <ricardo@ribalda.com>
2020-12-13 14:40:41 +0100

    2020/13: My own implementation of the chinesse reminder

--- 2020/13/ex2.py
+++ 2020/13/ex2.py
@@ -1,5 +1,6 @@
 import sys
 from functools import reduce
+import copy
 
 # from https://fangya.medium.com/chinese-remainder-theorem-with-python-a483de81fbb8
 
@@ -27,7 +28,7 @@ def mul_inv(a, b):
     return x1
 
 
-def find_val_fast(buses):
+def find_val_fast_chinesse(buses):
     n = list()
     a = list()
     for b in buses:
@@ -36,13 +37,39 @@ def find_val_fast(buses):
     return int(chinese_remainder(n, a))
 
 
+def find_pair(a, b):
+    # print(a,b)
+    step = a[0]
+    n = a[1]
+    while (n % b[0]) != b[1]:
+        n += step
+
+    # print(n)
+    r1 = n
+    r0 = a[0] * b[0]
+
+    return [r0, r1]
+
+
+def find_val_fast_ricardo(buses_in):
+    buses = copy.deepcopy(buses_in)
+    for i in range(len(buses)):
+        buses[i][1] = (buses[i][1] * -1) % buses[i][0]
+
+    a = buses[0]
+    for b in buses[1:]:
+        # print(a,b)
+        a = find_pair(a, b)
+    return a[1]
+
+
 def find_val_slow(buses):
     step = 1
     for b in buses:
         if b[1] == 0 and b[0] > step:
             step = b[0]
 
-    val = 1
+    val = 0
     while True:
         found = True
         for b in buses:
@@ -65,5 +92,6 @@ for l in sys.stdin.readlines():
             buses.append([int(b), offset])
         offset += 1
     print(buses)
-    print(find_val_fast(buses))
-    print(find_val_slow(buses))
+    print(find_val_fast_chinesse(buses))
+    print(find_val_fast_ricardo(buses))
+    # print(find_val_slow(buses))
