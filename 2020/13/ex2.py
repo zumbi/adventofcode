83fbe2162663f49e14daa1154dbec1b3a327311f
Ricardo Ribalda <ricardo@ribalda.com>
2020-12-13 14:46:27 +0100

    2020/13: Fix bug on chinese_remainder with big numbers

--- 2020/13/ex2.py
+++ 2020/13/ex2.py
@@ -9,7 +9,7 @@ def chinese_remainder(n, a):
     sum = 0
     prod = reduce(lambda a, b: a*b, n)
     for n_i, a_i in zip(n, a):
-        p = prod/n_i
+        p = prod // n_i
         sum += a_i * mul_inv(p, n_i)*p
     return sum % prod
 
