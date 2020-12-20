5aed8d4d8f10e38157ed5749e7e0b42ccdc8a6cd
Ricardo Ribalda <ricardo@ribalda.com>
2020-12-20 18:15:25 +0100

    2020/20: Exploit symetry

--- 2020/20/step1.py
+++ 2020/20/step1.py
@@ -1,10 +1,8 @@
 import sys
 import math
-import string
 
 
 def validate_last(pieces, size):
-    # print(pieces)
     if len(pieces) < 2:
         return True
 
@@ -22,14 +20,8 @@ def validate_last(pieces, size):
             return False
     return True
 
-#MAXORDER = 0
-
 
 def find_order(order_tiles, pieces, size):
-    # if len(order_tiles) > MAXORDER:
-    #    MAXORDER = len(order_tiles)
-    #    print(MAXORDER)
-    # print(len(order_tiles))
     if len(order_tiles) == (size * size):
         return order_tiles
     for t in pieces:
@@ -49,28 +41,18 @@ def rotate(edges):
     return out
 
 
-def flip_hor(edges):
-    out = []
-    out.append(edges[0])
-    out.append(edges[2])
-    out.append(edges[1])
-    out.append(edges[3])
-    return out
-
-
-def flip_ver(edges):
-    out = []
-    out.append(edges[3])
-    out.append(edges[1])
-    out.append(edges[2])
-    out.append(edges[0])
-    return out
+def flip_horver(edges):
+    return edges[::-1]
 
 
 def toint(a):
-    a = a.translate(string.maketrans('#.', "10"))
-    # a = a.translate(str.maketrans('#.','10'))
-    return int(a, 2)
+    b = ""
+    for s in a:
+        if s == "#":
+            b += "1"
+        else:
+            b += "0"
+    return int(b, 2)
 
 
 tiles = sys.stdin.read().split("\n\n")
@@ -93,21 +75,12 @@ for t in tiles:
         left += l[0]
         right += l[-1]
     edges = [top, right, down, left]
-    # pieces.append([n,list(map(toint,edges))])
+    # make use of symetry
     for i in range(4):
         pieces.append([n, list(map(toint, edges))])
-        pieces.append([n, list(map(toint, flip_hor(flip_ver(edges))))])
-        pieces.append([n, list(map(toint, flip_hor(edges)))])
-        pieces.append([n, list(map(toint, flip_ver(edges)))])
+        pieces.append([n, list(map(toint, flip_horver(edges)))])
         edges = rotate(edges)
 
-# for p in pieces:
-#    print(p)
-
 order = find_order([], pieces, SIZE)
-
-# for o in order:
-#    print(o)
 print(order)
-
 print(order[0][0]*order[SIZE-1][0]*order[-SIZE][0]*order[-1][0])
